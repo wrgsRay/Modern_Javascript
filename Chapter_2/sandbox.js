1732c3b03144519a29950ae4f9bd64bbcb956f55
Raymond Wang <raymond@raymond-mbp16.lan>
2020-06-14 22:10:53 -0700

    Initial commit

---
+++ Chapter_2/sandbox.js
@@ -0,0 +1,27 @@
+// Strings
+console.log('hello, world');
+
+let email = 'mario@test.com'
+
+console.log(email);
+
+// string concatenation
+let firstName = 'John';
+let lastName = 'Smith';
+
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+// getting characters
+console.log(fullName[0]);
+
+// string length
+console.log(fullName.length);
+
+// string methods
+console.log(fullName.toLocaleUpperCase());
+let result = fullName.toLocaleLowerCase();
+console.log(result)
+
+let index = email.indexOf('@')
+console.log(index)
