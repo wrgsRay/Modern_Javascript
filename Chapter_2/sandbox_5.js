f956429e11ddb898630ce1e77a4c8d45a8dff4c7
Raymond <r@wr.gs>
2020-06-15 18:20:48 -0700

    lesson continues

---
+++ Chapter_2/sandbox_5.js
@@ -0,0 +1,20 @@
+let ninjas = ['shaun', 'ryu', 'chun-li'];
+// ninjas[1] = 'ken'
+// console.log(ninjas[1])
+
+// let ages = [20, 25, 30, 35];
+// console.log(ages[2])
+
+// let random = ['shaun', 'crystal', 30, 20]
+// console.log(random)
+
+// console.log(ninjas.length)
+// array methods
+
+// let results = ninjas.join(',');
+// let result = ninjas.indexOf('chun-li')
+// let result = ninjas.concat(['ken', 'crystal'])
+// let result = ninjas.push('ken')
+// result = ninjas.pop();
+
+console.log(result)
