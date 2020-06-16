de249b8a38dfa6a2bbcfe640de6880885f02e73f
Raymond <r@wr.gs>
2020-06-15 18:54:53 -0700

    end of chapter 2

---
+++ Chapter_2/sandbox_7.js
@@ -0,0 +1,17 @@
+let age = 25;
+
+// loose comparision (different types can still be equal)
+
+// console.log(age == 25);
+// console.log(age == '25');
+
+// console.log(age != 25);
+// console.log(age != '25');
+
+// strict comparison (different types cannot be equal)
+
+console.log(age === 25);
+console.log(age === '25');
+
+console.log(age !== 25);
+console.log(age !== '25');
