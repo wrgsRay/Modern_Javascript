1f81704fcdfb1ad1b9570c8bbc11008495fb6314
Raymond Wang <28991499+wrgsRay@users.noreply.github.com>
2020-07-03 16:27:24 -0700

    Promise chaining

--- Chapter_12/sandbox_1.js
+++ Chapter_12/sandbox_1.js
@@ -1,30 +1,29 @@
-const getTodos = (resource, callback) => {
-    const request = new XMLHttpRequest()
+const getTodos = (resource) => {
+    return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest()
 
-    request.addEventListener('readystatechange', () => {
-        if (request.readyState === 4 && request.status === 200) {
-            const data = JSON.parse(request.responseText)
-            callback(undefined, data)
-        } else if(request.readyState === 4) {
-            callback('could not fetch data', undefined)
-        }
+        request.addEventListener('readystatechange', () => {
+            if (request.readyState === 4 && request.status === 200) {
+                const data = JSON.parse(request.responseText)
+                resolve(data)
+            } else if(request.readyState === 4) {
+                reject(`Error getting resource at ${resource}`)
+            }
+        })
+        request.open('GET', resource)
+        request.send()
     })
-    request.open('GET', resource)
-    request.send()
+
 }
 
-getTodos('json/luigi.json', (err, data) => {
-    console.log('call back fired')
-    // if(err){
-    //     console.log(err)
-    // } else {
-    //     console.log(data)
-    // }
+getTodos('json/luigi.json').then(data => {
     console.log(data)
-    getTodos('json/mario.json', (err, data) => {
-        console.log(data)
-        getTodos('json/shaun.json', (err, data) => {
-            console.log(data)
-        })
-    })
+    return getTodos('json/mario.json')
+}).then(data => {
+    console.log(data)
+    return getTodos('json/shaun.json')
+}).then(data => {
+    console.log(data)
+}).catch(err => {
+    console.log(err)
 })
