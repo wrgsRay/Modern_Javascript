de3ff3c8c266cfbcca58decb3afaba2e756d6b5c
Raymond Wang <28991499+wrgsRay@users.noreply.github.com>
2020-07-03 17:18:01 -0700

    async and await

--- Chapter_12/sandbox_2.js
+++ Chapter_12/sandbox_2.js
@@ -1,10 +1,27 @@
-// fetch api
+// async & await
 
-fetch('json/luigi.json').then((response) => {
-    console.log('resolved', response)
-    return response.json()
-}).then(data => {
-    console.log(data)
-}).catch((err => {
-    console.log('Network error', err)
-}))
+const getTodos = async () => {
+  const response = await fetch("json/luigis.json")
+
+  if (response.status !== 200) {
+    throw new Error('Cannot fetch data')
+  }
+  const data = await response.json();
+  return data;
+};
+
+
+const test = getTodos()
+    .then((data) => console.log(`resolved:`, data))
+    .catch(err => console.log('rejected:', err.message))
+
+
+
+// fetch('json/luigi.json').then((response) => {
+//     console.log('resolved', response)
+//     return response.json()
+// }).then(data => {
+//     console.log(data)
+// }).catch((err => {
+//     console.log('Network error', err)
+// }))
