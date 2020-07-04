4be882c2280499abf0b111538b9b9fd12f14dc5e
Raymond Wang <28991499+wrgsRay@users.noreply.github.com>
2020-07-04 16:46:35 -0700

    End of Chapter 13

---
+++ Chapter_13 Weather app/scripts/app.js
@@ -0,0 +1,61 @@
+const cityForm = document.querySelector("form");
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
+const time = document.querySelector("img.time");
+const icon = document.querySelector(".icon img");
+
+const updateUI = (data) => {
+  //   const cityDetails = data.cityDetails;
+  //   const weather = data.weather;
+
+  // destructure properties
+  const { cityDetails, weather } = data;
+
+  // update details template
+  details.innerHTML = `
+  <h5 class="my-3">${cityDetails.EnglishName}</h5>
+  <div class="my-3">${weather.WeatherText}</div>
+  <div class="display-4 my-4">
+      <span>${weather.Temperature.Metric.Value}</span>
+      <span>&deg;C</span>
+  </div>
+  `;
+  // update the night/day & icon images
+  const timeSrc = weather.IsDayTime ? "img/day.svg" : "img/night.svg";
+
+  //   let timeSrc = null;
+  //   if (weather.IsDayTime) {
+  //     timeSrc = "img/day.svg";
+  //   } else {
+  //     timeSrc = "img/night.svg";
+  //   }
+  time.setAttribute("src", timeSrc);
+
+  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+  icon.setAttribute("src", iconSrc);
+  // remove the d-none if present
+  if (card.classList.contains("d-none")) {
+    card.classList.remove("d-none");
+  }
+};
+
+const updateCity = async (city) => {
+  const cityDetails = await getCity(city);
+  const weather = await getWeather(cityDetails.Key);
+
+  return { cityDetails, weather };
+};
+
+cityForm.addEventListener("submit", (e) => {
+  // prevent default action
+  e.preventDefault();
+
+  // get city value
+  const city = cityForm.city.value.trim();
+  cityForm.reset();
+
+  //update the UI with new City
+  updateCity(city)
+    .then((data) => updateUI(data))
+    .catch((err) => console.log(err));
+});
---
+++ Chapter_13 Weather app/scripts/forecast.js
@@ -0,0 +1,32 @@
+const key = 'FdPGM4LPhH4cQBFZIwFYeGJTtvFrtXWQ'
+
+// Get weather info
+const getWeather = async (cityKey) => {
+    const baseURL = 'http://dataservice.accuweather.com/currentconditions/v1/'
+    query = `${cityKey}?apikey=${key}`
+
+    const response = await fetch(baseURL + query)
+    const data = await response.json()
+
+    return data[0]
+}
+
+// Get City info
+const getCity = async (city) => {
+
+    const baseURL = 'http://dataservice.accuweather.com/locations/v1/cities/search'
+    const query = `?apikey=${key}&q=${city}`
+
+    const response = await fetch(baseURL + query)
+    const data = await response.json()
+
+    return data[0]
+}
+
+// getCity('manchester').then(data => {
+//     return getWeather(data.Key)
+// }).then(data => {
+//     console.log(data)
+// })
+// .catch(err => console.log(err))
+
