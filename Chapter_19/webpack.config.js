78d3118e5941280d50a41e1253380d3fec60a2e7
Raymond Wang <28991499+wrgsRay@users.noreply.github.com>
2020-07-14 22:02:04 -0700

    End of Chapter 19

--- Chapter_19/webpack.config.js
+++ Chapter_19/webpack.config.js
@@ -5,5 +5,21 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist/assets'),
         filename: 'bundle.js'
+    },
+    devServer:{
+        contentBase: path.resolve(__dirname, 'dist'),
+        publicPath: '/assets/'
+    },
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        }]
     }
 };
