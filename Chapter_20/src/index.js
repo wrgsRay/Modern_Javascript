9715f8a96ecce9677146f7c5e9a3798f5a96901a
Raymond Wang <28991499+wrgsRay@users.noreply.github.com>
2020-07-18 23:36:19 -0700

    end of chapter 20

---
+++ Chapter_20/src/index.js
@@ -0,0 +1,29 @@
+import Tooltip from './ninja-ui/tooltip'
+import Dropdown from './ninja-ui/dropdown'
+import Tabs from './ninja-ui/tabs'
+import Snackbar from './ninja-ui/snackbar'
+
+//create a tooltip
+const tooltip = new Tooltip(document.querySelector('.tooltip'))
+tooltip.init()
+
+// create dropdown instances
+const dropdowns = document.querySelectorAll('.dropdown')
+
+dropdowns.forEach(dropdown => {
+    const instance = new Dropdown(dropdown)
+    instance.init()
+})
+
+// create tabs
+const tabs = new Tabs(document.querySelector('.tabs'))
+tabs.init()
+
+// create snack bar
+const snackbar = new Snackbar();
+snackbar.init()
+
+const button = document.querySelector('.snackbar-trigger')
+button.addEventListener('click', () =>{
+    snackbar.show('you clicked me :)')
+})
